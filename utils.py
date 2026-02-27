17c4df3a94755c10686c07fbd7caebcd5d88cabe
sofiiamalyshevska <sofiiamalyshevska@knu.ua>
2026-02-27 19:56:22 +0200

    Remove is_prime function from utils.py
    Removed is_prime function and adjusted formatting.

--- utils.py
+++ utils.py
@@ -2,17 +2,14 @@ def factorial(n):
     if n == 0 or n == 1:
         return 1
     return n * factorial(n - 1)
-def is_prime(n):
-    if n < 2: return False
-    for i in range(2, int(n**0.5) + 1):
-        if n % i == 0: return False
-    return True
+
 def nsd(a,b):
     a=abs(a)
     b=abs(b)
     while b!=0:
         a,b=b,a%b
     return a
+    
 def fibonacci(n):
     """Повертає n-те число Фібоначчі"""
     if n <= 0:
