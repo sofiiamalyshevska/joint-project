c1ea5eccd80c770517da3ee7527bcf7bfb21022f
AliceArtiukh <alisa_artiukh1@knu.ua>
2026-02-27 18:42:27 +0200

    NSD function

--- utils.py
+++ utils.py
@@ -7,3 +7,11 @@ def is_prime(n):
     for i in range(2, int(n**0.5) + 1):
         if n % i == 0: return False
     return True
+a=int(input())
+b=int(input())
+x,y=a,b
+while y:
+    x,y=y,x%y
+nsd=x
+print(nsd)
+
