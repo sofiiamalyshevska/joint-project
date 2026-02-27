485db00424757b25d16c51ac53065bee6dd47e0c
Людмила <lyudosshka_@knu.ua>
2026-02-27 19:36:13 +0200

    Fixed typo in import name

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from utils import factorial
 print(f"Factorial of 8: {factorial(8)}")
-from utils import is_pover_of_five
+from utils import is_power_of_five
 print(is_power_of_five(125))
 from utils import nsd
 print(f"Factorial of 8: {factorial(8)}")
