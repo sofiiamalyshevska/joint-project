a90fe6bf47dbcea73b5a929265ba423340a9bb22
Людмила <lyudosshka_@knu.ua>
2026-02-27 19:45:16 +0200

    Improved output message for power of five

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from utils import factorial
 print(f"Factorial of 8: {factorial(8)}")
 from utils import is_power_of_five
-print(is_power_of_five(125))
+print(f"is 125 power of five? {is_power_of_five(125)}")
 from utils import nsd
 print(f"Factorial of 8: {factorial(8)}")
 print(nsd(84,30)) 
