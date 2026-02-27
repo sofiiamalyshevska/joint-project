e1bca83f85b61e986170c6c185478cb958f5dcf6
milienakolot2025 <260503702+milienakolot2025@users.noreply.github.com>
2026-02-27 19:13:23 +0200

    Added fibonacci function

--- utils.py
+++ utils.py
@@ -14,6 +14,19 @@ while y:
     x,y=y,x%y
 nsd=x
 print(nsd)
+def fibonacci(n):
+    """Повертає n-те число Фібоначчі"""
+    if n <= 0:
+        return None
+    if n == 1:
+        return 0
+    if n == 2:
+        return 1
+    a, b = 0, 1
+    for _ in range(2, n):
+        a, b = b, a + b
+    return b
+
 
 def is_power_of_five(n):
     if n <= 0:
