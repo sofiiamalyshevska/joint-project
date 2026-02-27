0e67a827b75c0268bb487d9d10e370b821191ee5
AliceArtiukh <alisa_artiukh1@knu.ua>
2026-02-27 19:39:19 +0200

    Delete

--- main.py
+++ main.py
@@ -3,6 +3,5 @@ print(f"Factorial of 8: {factorial(8)}")
 from utils import is_power_of_five
 print(is_power_of_five(125))
 from utils import nsd
-print(f"Factorial of 8: {factorial(8)}")
 print(nsd(84,30)) 
 
