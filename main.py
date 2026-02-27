0d2dee6086c49f19cc72b358e7dd65e7a634e7ec
milienakolot2025 <260503702+milienakolot2025@users.noreply.github.com>
2026-02-27 19:34:37 +0200

    Added fibonacci function call in main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from utils import factorial
 from utils import nsd
+from utils import fibonacci
 
 print(f"Factorial of 8: {factorial(8)}")
-print(nsd(84,30)) 
+print(nsd(84,30))
+print(f"10-th Fibonacci number is: {fibonacci(10)}")
